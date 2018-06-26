bcda4614bfebc6065e6864f00fd276eafd37f825
BreethiSundar21 <40389885+BreethiSundar21@users.noreply.github.com>
2018-06-26 09:58:58 +0000

    Rename odd.c to Odd.c

---
+++ code kata/Odd.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if((N>1)&&(N<100000))
+    {
+        if(N%2==0)
+        {
+            printf("EVEN");
+        }
+        else
+        {
+            printf("ODD");
+        }
+    }
+    else
+    {
+        printf("Invalid input");
+    }
+    return 0;
+    
+}
