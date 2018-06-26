83f2dd17e6b8b5af323c66490689fa23031c7f83
BreethiSundar21 <40389885+BreethiSundar21@users.noreply.github.com>
2018-06-26 11:10:46 +0000

    Update array.c

--- code kata/array.c
+++ code kata/array.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a[5],b,i,sum=0;
-    scanf("%d",&b);
+    int a[5],b,c,i,sum=0;
+    scanf("%d%d%d",&a,&b,&c);
     for(i=0;i<=b;i++)
     {
         sum=sum+i;
